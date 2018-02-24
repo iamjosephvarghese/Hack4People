6233aa109745a14fa2b5af244d97d5d12183f1de
iamjosephvarghese <josephvarghese.rms@gmail.com>
2018-02-24 19:31:51 +0530

    starting work on ads view...and adapter

---
+++ app/src/main/java/hackathon/rm/com/hack4people/Ads.java
@@ -0,0 +1,13 @@
+package hackathon.rm.com.hack4people;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ads extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ads);
+    }
+}
---
+++ app/src/main/java/hackathon/rm/com/hack4people/AdsAdapter.java
@@ -0,0 +1,8 @@
+package hackathon.rm.com.hack4people;
+
+/**
+ * Created by josephvarghese on 24/02/18.
+ */
+
+public class AdsAdapter {
+}
